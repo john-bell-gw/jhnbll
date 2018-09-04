99197b4a305b5462453010e8554f8ac3b307e3e6
john.bell <john.bell@e2x.co.uk>
2018-09-04 14:23:14 +0100

    changing some content and header colour

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>I done a website</h1>
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
