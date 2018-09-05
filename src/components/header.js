1747840d99475be44100ae54beec56a10d305b01
john.bell <john.bell@e2x.co.uk>
2018-09-05 09:31:17 +0100

    hotpink

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: '#138B91',
+      background: 'hotpink',
       marginBottom: '1.45rem',
     }}
   >
